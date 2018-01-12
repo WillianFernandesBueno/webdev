626858d870cb0948e538357a8682e8d1cceee6a5
douglasqueiroz <douglasqueirozalmeida@gmail.com>
2018-01-12 15:51:08 -0200

    configuracoes mobile

--- webdev-mobile/mobile/src/pages/home/home.ts
+++ webdev-mobile/mobile/src/pages/home/home.ts
@@ -1,16 +1,18 @@
 import { ComunicaBackendProvider } from '../../providers/comunica-servidor-provider';
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
 
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [ComunicaBackendProvider]
+  providers: [ComunicaBackendProvider,GooglePlus]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public comunicaBackendProvider: ComunicaBackendProvider) {
+  constructor(public navCtrl: NavController, public comunicaBackendProvider: ComunicaBackendProvider, public googlePlus: GooglePlus) {
+    this.login();
     
   }
 
@@ -23,4 +25,36 @@ export class HomePage {
     alert("sucesso");
   }
 
+  login() {
+    this.googlePlus.login({})
+      .then(res => {
+        console.log(res);
+        // this.displayName = res.displayName;
+        // this.email = res.email;
+        // this.familyName = res.familyName;
+        // this.givenName = res.givenName;
+        // this.userId = res.userId;
+        // this.imageUrl = res.imageUrl;
+
+        // this.isLoggedIn = true;
+        console.log("logou");
+      })
+      .catch(err => console.error(err));
+  }
+
+  logout() {
+    this.googlePlus.logout()
+      .then(res => {
+        console.log(res);
+        // this.displayName = "";
+        // this.email = "";
+        // this.familyName = "";
+        // this.givenName = "";
+        // this.userId = "";
+        // this.imageUrl = "";
+
+        // this.isLoggedIn = false;
+      })
+      .catch(err => console.error(err));
+  }
 }
